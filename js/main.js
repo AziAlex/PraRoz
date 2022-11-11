335485502b73b637b53b4406aab9579b7e14e6c9
Alex <102092661+AziAlex@users.noreply.github.com>
2022-11-11 10:41:52 +0200

    first

---
+++ js/main.js
@@ -0,0 +1,4 @@
+document.querySelector(".menu").addEventListener("click", function(){
+    document.querySelector(".nav").classList.toggle("active")
+    document.querySelector(".menu").classList.toggle("active")
+})
